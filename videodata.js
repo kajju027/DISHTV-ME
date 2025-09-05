5b8d53afda69c4662294aa0163a5c4e5b69a69e6
SAYAN X MD <kajju3864@gmail.com>
2025-09-05 09:09:10 +0530

    Update videodata.js

--- videodata.js
+++ videodata.js
@@ -1,2 +1,2 @@
 // videoData.js
-const base64VideoUrl = "aHR0cHM6Ly90dmdvLm5lb2NpdGllcy5vcmcvQVBQVFYvamlvaGQ/aWQ9MTM3Mw=="; // Base64 encoded link
+const base64VideoUrl = "aHR0cHM6Ly90dmdvLm5lb2NpdGllcy5vcmcvQVBQVFYvamlvaGQ/aWQ9MTM3Mw=="; 
